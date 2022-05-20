d1a24fc1561be4f755a9477936abf5794ac1834b
nlenno1 <nick.lennon@hotmail.co.uk>
2022-05-20 13:59:55 +0000

    feat: add connectionrequest app and files structure for static, media and templates

---
+++ connectionrequest/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConnectionrequestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'connectionrequest'
--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A view to return the about us page """
+
+    return render(request, 'home/about-us.html')
+
+
+def how_to_use(request):
+    """ A view to return the how to use page """
+
+    return render(request, 'home/how-to-use.html')
