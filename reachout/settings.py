be9830609aa33c203122ff0f42928b71b8e75e10
nlenno1 <nick.lennon@hotmail.co.uk>
2022-05-20 15:18:36 +0000

    feat: add environ to seeting to contain db password in env.py

--- reachout/settings.py
+++ reachout/settings.py
@@ -11,12 +11,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DB_SECRET_KEY = os.environ.get("DB_SECRET_KEY", "")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
