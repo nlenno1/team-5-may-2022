8d76cc80f4dcb894da9bfa885790b829e10ae6c8
Abhisek Joshi <nofursad@gmail.com>
2022-05-21 09:37:10 +0000

    moved secret key to .env and changed workbench theme.

--- reachout/settings.py
+++ reachout/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_7s#fzitjss9w(f_)y=_ki4r8v)n^0yoa-m^(-$k)ikio^2o&'
+SECRET_KEY =  env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
