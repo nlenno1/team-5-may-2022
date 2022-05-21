03fec1bace303c70224a333da19b0ac97212601b
nlenno1 <nick.lennon@hotmail.co.uk>
2022-05-21 16:57:11 +0000

    fix: fix conditional error

--- reachout/settings.py
+++ reachout/settings.py
@@ -10,16 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
 import os
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
-
-# # Initialise environment variables
-# env = environ.Env()
-# # reading .env file
-# environ.Env.read_env()
 
 DB_SECRET_KEY = os.environ.get("DB_SECRET_KEY")
 
@@ -194,17 +188,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email set up
-
-if "DEVELOPMENT" in env:
+if os.environ.get("DEVELOPMENT") == "True":
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     DEFAULT_FROM_EMAIL = "reachout@example.com"
-    print("DEV")
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASS")
-    DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER")
-    print("LIVE")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
