c79eb732623308a001446a4ffaad335fadea4f5a
nlenno1 <nick.lennon@hotmail.co.uk>
2022-05-21 10:59:05 +0000

    feat: add POST for send connection request

--- connectionrequest/views.py
+++ connectionrequest/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from .forms import ConnectionRequestForm
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
 def send_connection_request(request):
     """ A view to return the connection request page """
 
     form = ConnectionRequestForm()
+
+    if request.method == "POST":
+        form = ConnectionRequestForm(request.POST)
+        if form.is_valid():
+            new_connection_request = form.save()
+            print("NEW CONNECTION REQUEST CREATED")
+            print(new_connection_request.request_id)
+            # messages.success(
+            #     request,
+            #     f"Instructor Profile for \
+            #                 {instructor.friendly_name} created",
+            # )
+        else:
+            # messages.error(
+            #     request,
+            #     "Unable to Instructor Profile. Please check \
+            #     that the form is valid",
+            # )
+            print("CONNECTION REQUEST FAILED")
+
+        return redirect(reverse("send_connection_request"))
 
     context = {
         'form': form,
