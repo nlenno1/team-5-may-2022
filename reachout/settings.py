19205f66d04b301e871bb068eee4510f2678a304
JoyZadan <joyzadan.learning@gmail.com>
2022-05-22 21:50:13 +0000

    Fix: database config for workspace and tested css on allauth form

--- reachout/settings.py
+++ reachout/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,11 +30,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-if development:
-    DEBUG = True
-else:
-    DEBUG = False
 
 if development:
     ALLOWED_HOSTS = ['localhost']
