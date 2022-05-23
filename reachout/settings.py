3d9be3dad6febb6cdcc8549d58a2d3d202c8b90c
nlenno1 <nick.lennon@hotmail.co.uk>
2022-05-23 11:12:23 +0000

    fix: fix allauth templating error and add conditional to set development variable

--- reachout/settings.py
+++ reachout/settings.py
@@ -16,18 +16,16 @@ from pathlib import Path
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', True)
+development = True if os.environ.get("DEVELOPMENT")=="True" else False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -77,9 +75,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # os.path.join(BASE_DIR, 'templates'),
-            # os.path.join(BASE_DIR, 'templates', 'allauth'),
-            TEMPLATES_DIR
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -135,18 +132,6 @@ else:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'reachout',
-#         'ENFORCE_SCHEMA': False,
-#         'CLIENT': {
-#             'host': f'mongodb+srv://reachout-dev:{DB_SECRET_KEY}@myfirstcluster.exgjq.mongodb.net/?retryWrites=true&w=majority'
-#         }
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -198,9 +183,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email set up
-if os.environ.get("DEVELOPMENT") == "True":
+if development:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     DEFAULT_FROM_EMAIL = "reachout@example.com"
+    print("dev server")
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
